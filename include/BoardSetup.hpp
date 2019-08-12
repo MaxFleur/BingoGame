11bcf3450393ae6c3cd069569d05060bb0257b0f
Maxime Fleury <s1024157@mail.zih.tu-dresden.de>
2019-08-12 14:36:55 +0200

    Moves setup to BoardSetup, restructered BoardSetup members to private

--- include/BoardSetup.hpp
+++ include/BoardSetup.hpp
@@ -9,11 +9,39 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "CinderOpenCv.h"
+#include "cinder/audio/audio.h"
+
+using namespace ci;
 
 class BoardSetup {
 public:
 	BoardSetup() {};
 	~BoardSetup() {};
+
+	void setup() {
+		std::string header = "DAS EPISCHSTE BULLSHIT-BINGO DER WELT!";
+		TextBox tBoxSetup = TextBox().alignment(TextBox::CENTER).font(Font("Helvetica", 40)).size(ivec2(700, 40)).text(header);
+		tBoxSetup.setColor(Color(0.03f, 0.03f, 0.03f));
+		tBoxSetup.setBackgroundColor(Color(0.85f, 0.55f, 0.32f));
+		headerTexture = gl::Texture2d::create(tBoxSetup.render());
+
+		tBoxSetup.setText("YEAH, DU HAST GEWONNEN! \nABER LEIDER GIBTS KEINEN PREIS. \nSPIEL DOCH EINFACH NOCHMAL! \nKLICKE, UM DAS SPIEL NEUZUSTARTEN.");
+		tBoxSetup.setFont(Font("Helvetica", 32));
+		tBoxSetup.setSize(vec2(500, 120));
+		tBoxSetup.setColor(Color(1.0f, 0.839f, 0.0f));
+		tBoxSetup.setBackgroundColor(Color(0.289f, 0.125f, 0.23f));
+		winningTexture = gl::Texture2d::create(tBoxSetup.render());
+
+		tBoxSetup.setText("Neustarten");
+		tBoxSetup.setSize(vec2(150, 40));
+		tBoxSetup.setColor(Color(0.03f, 0.03f, 0.03f));
+		tBoxSetup.setBackgroundColor(Color(0.96f, 0.96f, 0.96f));
+		restartTexture = gl::Texture2d::create(tBoxSetup.render());
+
+		ci::audio::SourceFileRef sourceFile = audio::load(app::loadAsset("win.mp3"));
+		mVoice = audio::Voice::create(sourceFile);
+		mVoice->setVolume(8);
+	}
 
 	cv::Mat createBoard(cv::Mat input, std::vector<std::string> entrys) {
 		// Clear and reserve
@@ -43,22 +71,22 @@ public:
 					textSize = 32;
 				}
 
-				textBox.setFont(ci::Font("Helvetica", textSize));
+				textBox.setFont(Font("Helvetica", textSize));
 				textBox.setText(entrys.at(entryIndex));
 				// Black background and white text for centered field
 				if (x == 2 && y == 2) {
 					isBlack[x].push_back(true);
-					textBox.setColor(ci::Color(0.96f, 0.96f, 0.96f));
-					textBox.setBackgroundColor(ci::Color(0.03f, 0.03f, 0.03f));
+					textBox.setColor(Color(0.96f, 0.96f, 0.96f));
+					textBox.setBackgroundColor(Color(0.03f, 0.03f, 0.03f));
 				}
 				// White background and black text for all other fields
 				else {
 					isBlack[x].push_back(false);
-					textBox.setColor(ci::Color(0.0f, 0.0f, 0.0f));
-					textBox.setBackgroundColor(ci::Color(0.96f, 0.96f, 0.96f));
+					textBox.setColor(Color(0.0f, 0.0f, 0.0f));
+					textBox.setBackgroundColor(Color(0.96f, 0.96f, 0.96f));
 				}
 				// Create a texture for every testbox and store it for later drawing
-				ci::gl::TextureRef Texture = ci::gl::Texture2d::create(textBox.render());
+				gl::TextureRef Texture = gl::Texture2d::create(textBox.render());
 				fieldTextures[x].push_back(Texture);
 
 				// Draws the square meshes over the field
@@ -78,9 +106,30 @@ public:
 		return input;
 	}
 
+	std::vector<std::vector<ci::gl::TextureRef>> getFieldTextures() { return fieldTextures; }
+	TextBox getTextBox() { return textBox; }
+	std::vector<std::vector<bool>> getBlackFields() { return isBlack; }
+
+	bool getRestart() { return restart; }
+	gl::TextureRef getWinTexture() { return winningTexture; }
+	gl::TextureRef getRestartTexture() { return restartTexture; }
+	gl::TextureRef getHeaderTexture() { return headerTexture; }
+	// Winning sound
+	audio::VoiceRef getVoice() { return mVoice; }
+
+	void setRestart(bool newRestart) { restart = newRestart; }
+
+private:
+
 	std::vector<std::vector<ci::gl::TextureRef>> fieldTextures;
-	ci::TextBox textBox;
+	TextBox textBox;
 	// Stores fields clicked on
 	std::vector<std::vector<bool>> isBlack;
+
+	bool restart;
+	gl::TextureRef winningTexture;
+	gl::TextureRef restartTexture;
+	gl::TextureRef headerTexture;
+	audio::VoiceRef mVoice;
 };
 using BoardCreatorRef = std::shared_ptr<BoardSetup>;
--- src/BingoGameApp.cpp
+++ src/BingoGameApp.cpp
@@ -9,7 +9,6 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "cinder/ImageIo.h"
-#include "cinder/audio/audio.h"
 #include <algorithm>
 
 #include "Randomizer.hpp"
@@ -22,24 +21,15 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-class BingoGameApp : public App {
+class BingoGameApp : public App, public BoardSetup {
 public:
 	void setup() override;
 	void draw() override;
 
 	void mouseUp(MouseEvent event) override;
 
-	bool restart;
-
-	// 2D-Vector for the textBoxes used over the bingo board
-	vector<vector<gl::TextureRef>> texturesFromTextBoxes;
 	// Texture for the ground and the square meshes
 	gl::TextureRef mTexture;
-	gl::TextureRef winningTexture;
-	gl::TextureRef restartTexture;
-	gl::TextureRef headerTexture;
-
-	audio::VoiceRef mVoice;
 
 	RandomizerRef r;
 	BoardCreatorRef bS;
@@ -52,35 +42,13 @@ void BingoGameApp::setup()
 	bS		= std::make_shared<BoardSetup>();
 	bLS		= std::make_shared<BlackLineSearch>();
 
-	string header = "DAS EPISCHSTE BULLSHIT-BINGO DER WELT!";
-	TextBox tBoxSetup = TextBox().alignment(TextBox::CENTER).font(Font("Helvetica", 40)).size(ivec2(700, 40)).text(header);
-	tBoxSetup.setColor(Color(0.03f, 0.03f, 0.03f));
-	tBoxSetup.setBackgroundColor(Color(0.85f, 0.55f, 0.32f));
-	headerTexture = gl::Texture2d::create(tBoxSetup.render());
-
-	tBoxSetup.setText("YEAH, DU HAST GEWONNEN! \nABER LEIDER GIBTS KEINEN PREIS. \nSPIEL DOCH EINFACH NOCHMAL! \nKLICKE, UM DAS SPIEL NEUZUSTARTEN.");
-	tBoxSetup.setFont(Font("Helvetica", 32));
-	tBoxSetup.setSize(vec2(500, 120));
-	tBoxSetup.setColor(Color(1.0f, 0.839f, 0.0f));
-	tBoxSetup.setBackgroundColor(Color(0.289f, 0.125f, 0.23f));
-	winningTexture = gl::Texture2d::create(tBoxSetup.render());
-
-	tBoxSetup.setText("Neustarten");
-	tBoxSetup.setSize(vec2(150, 40));
-	tBoxSetup.setColor(Color(0.03f, 0.03f, 0.03f));
-	tBoxSetup.setBackgroundColor(Color(0.96f, 0.96f, 0.96f));
-	restartTexture = gl::Texture2d::create(tBoxSetup.render());
-
-	audio::SourceFileRef sourceFile = audio::load(app::loadAsset("win.mp3"));
-	mVoice = audio::Voice::create(sourceFile);
-	mVoice->setVolume(8);
-
 	// Load Board image and create a matrix
 	ci::Surface8u surface(loadImage(loadAsset("BoardGround.jpg")));
 	cv::Mat input(toOcv(surface));
 
 	// randomizes the strings and draws square meshes over the board
 	r->randomize();
+	bS->setup();
 	cv::Mat output = bS->createBoard(input, r->getEntrys());
 
 	// Create a texture from all stuff and set the windows to the actual board size
@@ -91,9 +59,9 @@ void BingoGameApp::setup()
 void BingoGameApp::mouseUp(MouseEvent event) {
 
 	if (event.isLeft()) {
-		if (restart == true) {
+		if (bS->getRestart() == true) {
 			setup();
-			restart = false;
+			bS->setRestart(false);
 		}
 		else {
 			float x = event.getX();
@@ -108,19 +76,19 @@ void BingoGameApp::mouseUp(MouseEvent event) {
 
 				int cloneIndex = (boxRow * 5) + boxCol;
 
-				bS->isBlack.at(boxRow).at(boxCol) = true;
+				bS->getBlackFields().at(boxRow).at(boxCol) = true;
 
-				bS->textBox.setText(r->getEntrys().at(cloneIndex));
-				bS->textBox.setColor(Color(0.96f, 0.96f, 0.96f));
-				bS->textBox.setBackgroundColor(Color(0.03f, 0.03f, 0.03f));
+				bS->getTextBox().setText(r->getEntrys().at(cloneIndex));
+				bS->getTextBox().setColor(Color(0.96f, 0.96f, 0.96f));
+				bS->getTextBox().setBackgroundColor(Color(0.03f, 0.03f, 0.03f));
 
-				gl::TextureRef Texture = gl::Texture2d::create(bS->textBox.render());
-				bS->fieldTextures.at(boxRow).at(boxCol) = Texture;
-				gl::draw(bS->fieldTextures[boxRow][boxCol]);
+				gl::TextureRef Texture = gl::Texture2d::create(bS->getTextBox().render());
+				bS->getFieldTextures().at(boxRow).at(boxCol) = Texture;
+				gl::draw(bS->getFieldTextures()[boxRow][boxCol]);
 				
-				if (bLS->searchForBlackLine(bS->isBlack)) {
-					mVoice->start();
-					restart = true;
+				if (bLS->searchForBlackLine(bS->getBlackFields())) {
+					bS->getVoice()->start();
+					bS->setRestart(true);
 				}
 			}
 			if (x > 375 && x < 525 && y > 925 && y < 965) {
@@ -140,17 +108,17 @@ void BingoGameApp::draw()
 	for (int x = 0; x <= 4; x++) {
 		int width = 51;
 		for (int y = 0; y <= 4; y++) {
-			gl::draw(bS->fieldTextures[x][y], vec2(width, height));
+			gl::draw(bS->getFieldTextures()[x][y], vec2(width, height));
 			width += 160;
 		}
 		height += 160;
 	}
 
-	gl::draw(restartTexture, vec2(375, 925));
-	gl::draw(headerTexture, vec2(100, 20));
+	gl::draw(bS->getRestartTexture(), vec2(375, 925));
+	gl::draw(bS->getHeaderTexture(), vec2(100, 20));
 
-	if (bLS->searchForBlackLine(bS->isBlack)) {
-		gl::draw(winningTexture, vec2(200, 130));
+	if (bLS->searchForBlackLine(bS->getBlackFields())) {
+		gl::draw(bS->getWinTexture(), vec2(200, 130));
 	}
 }
 
